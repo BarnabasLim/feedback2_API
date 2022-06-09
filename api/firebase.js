e378ee384ed64262fcaf61971323f2e0fb1f1ca1
BarnabasLim <blim048@e.ntu.edu.sg>
2022-06-09 15:45:17 +0800

    key

---
+++ api/firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyAFTWx-xUbXwjUQeEFfsdBiUcyP5CTCxe8",
+    authDomain: "feedback-d3cb5.firebaseapp.com",
+    projectId: "feedback-d3cb5",
+    storageBucket: "feedback-d3cb5.appspot.com",
+    messagingSenderId: "383274450128",
+    appId: "1:383274450128:web:c3ecf559fe5031b71eb2d5",
+    measurementId: "G-H2Y3FPPJLN"
+  };
+  
